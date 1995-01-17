cb761a8d6cb45990aafcc7e70d39af68a1dc7018
nobody <nobody@6015fed2-1504-0410-9fe1-9d1591cc4771>
1995-01-17 17:01:40 +0000

    This commit was manufactured by cvs2svn to create tag 'r12beta2'.

---
+++ python/tags/r12beta2/Mac/Include/macdefs.h
@@ -0,0 +1,32 @@
+/* Useful #includes and #defines for programming a set of Unix
+   look-alike file system access functions on the Macintosh.
+   Public domain by Guido van Rossum, CWI, Amsterdam (July 1987).
+*/
+
+#include <Types.h>
+#include <Files.h>
+#include <OSUtils.h>
+
+#ifdef THINK_C
+#include <pascal.h>
+#endif
+
+#ifdef __MWERKS__
+#include "errno_unix.h"
+#endif
+#include <errno.h>
+#include <string.h>
+
+/* Macro to find out whether we can do HFS-only calls: */
+#define FSFCBLen (* (short *) 0x3f6)
+#define hfsrunning() (FSFCBLen > 0)
+
+/* Universal constants: */
+#define MAXPATH 256
+#define TRUE 1
+#define FALSE 0
+#ifndef NULL
+#define NULL 0
+#endif
+#define EOS '\0'
+#define SEP ':'
---
+++ python/tags/r12beta2/Mac/Include/macglue.h
@@ -0,0 +1,3 @@
+char *macstrerror Py_PROTO((int));			/* strerror with mac errors */
+PyObject *PyErr_Mac Py_PROTO((PyObject *, int));	/* Exception with a mac error */
+int PyMac_Idle Py_PROTO((void));			/* Idle routine */
