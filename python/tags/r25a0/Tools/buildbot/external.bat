1861b46bf9ff0b5a0d961c8eb35c91f1efa263f7
anthony.baxter <anthony.baxter@6015fed2-1504-0410-9fe1-9d1591cc4771>
2006-03-20 06:25:04 +0000

    experimenting with release stuff. please ignore

---
+++ python/tags/r25a0/Tools/buildbot/external.bat
@@ -0,0 +1,14 @@
+@rem Fetches (and builds if necessary) external dependencies
+
+@rem Assume we start inside the Python source directory
+cd ..
+call "%VS71COMNTOOLS%vsvars32.bat"
+
+@rem bzip
+if not exist bzip2-1.0.3 svn export http://svn.python.org/projects/external/bzip2-1.0.3
+
+@rem Sleepycat db
+if not exist db-4.4.20 svn export http://svn.python.org/projects/external/db-4.4.20
+if not exist db-4.4.20\build_win32\debug\libdb44sd.lib (
+   devenv db-4.4.20\build_win32\Berkeley_DB.sln /build Debug /project db_static
+)
