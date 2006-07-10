dafa1103a1c0dd18c4962a31ce2189f4b07ecef1
tim.peters <tim.peters@6015fed2-1504-0410-9fe1-9d1591cc4771>
2006-07-10 21:37:04 +0000

    This branch was merged to the trunk.

--- python/branches/tim-current_frames/Lib/test/crashers/bogus_code_obj.py
+++
@@ -1,9 +0,0 @@
-"""
-Broken bytecode objects can easily crash the interpreter.
-"""
-
-import types
-
-co = types.CodeType(0, 0, 0, 0, '\x04\x71\x00\x00', (),
-                    (), (), '', '', 1, '')
-exec co
