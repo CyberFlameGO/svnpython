3b89b94e2903a4a3825d5bf910d348533ec2400b
benjamin.peterson <benjamin.peterson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-08-21 01:30:15 +0000

    kill an old branch

--- python/branches/trunk-math/Tools/buildbot/build.bat
+++
@@ -1,6 +0,0 @@
-@rem Used by the buildbot "compile" step.
-cmd /c Tools\buildbot\external.bat
-call "%VS90COMNTOOLS%vsvars32.bat"
-cmd /q/c Tools\buildbot\kill_python.bat
-vcbuild /useenv PCbuild\pcbuild.sln "Debug|Win32"
-
--- python/branches/trunk-math/Tools/buildbot/clean-amd64.bat
+++
@@ -1,7 +0,0 @@
-@rem Used by the buildbot "clean" step.
-call "%VS90COMNTOOLS%vsvars32.bat"
-cd PCbuild
-@echo Deleting .pyc/.pyo files ...
-del /s Lib\*.pyc Lib\*.pyo
-vcbuild /clean pcbuild.sln "Release|x64"
-vcbuild /clean pcbuild.sln "Debug|x64"
