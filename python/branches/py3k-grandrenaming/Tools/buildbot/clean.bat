72d8be0445b64668cfde81f53fb7b46935275e73
benjamin.peterson <benjamin.peterson@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-08-21 01:28:07 +0000

    kill an old branch

--- python/branches/py3k-grandrenaming/Tools/buildbot/clean.bat
+++
@@ -1,7 +0,0 @@
-@rem Used by the buildbot "clean" step.
-call "%VS71COMNTOOLS%vsvars32.bat"
-cd PC\VS7.1
-@echo Deleting .pyc/.pyo files ...
-del /s Lib\*.pyc Lib\*.pyo
-devenv.com /clean Release pcbuild.sln
-devenv.com /clean Debug pcbuild.sln
--- python/branches/py3k-grandrenaming/Tools/buildbot/test.bat
+++
@@ -1,3 +0,0 @@
-@rem Used by the buildbot "test" step.
-cd PC\VS7.1
-call rt.bat -d -q -uall -rw -n
