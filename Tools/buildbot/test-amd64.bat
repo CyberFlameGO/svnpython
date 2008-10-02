1b22350c2bae7e4f3794816f917b486aa171df64
martin.v.loewis <martin.v.loewis@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-10-02 12:15:43 +0000

    Create a nearly-empty Python project for cleaning up build slaves.

--- Tools/buildbot/test-amd64.bat
+++ Tools/buildbot/test-amd64.bat
@@ -1,3 +1,1 @@
-@rem Used by the buildbot "test" step.
-cd PCbuild
-call rt.bat -q -d -x64 -uall -rw
+@echo Done
--- Tools/buildbot/test.bat
+++ Tools/buildbot/test.bat
@@ -1,3 +1,1 @@
-@rem Used by the buildbot "test" step.
-cd PCbuild
-call rt.bat -d -q -uall -rw
+@echo Done
