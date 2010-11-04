3f46e78496ac35c72d664a3366344af48b8ce2b3
hirokazu.yamamoto <hirokazu.yamamoto@6015fed2-1504-0410-9fe1-9d1591cc4771>
2010-11-04 13:47:05 +0000

    Removed because no more needed.

--- python/branches/release27-maint-ttk-debug-on-xp5/Tools/buildbot/build.bat
+++
@@ -1,6 +0,0 @@
-@rem Used by the buildbot "compile" step.
-cmd /c Tools\buildbot\external.bat
-call "%VS90COMNTOOLS%vsvars32.bat"
-cmd /c Tools\buildbot\clean.bat
-vcbuild /useenv PCbuild\pcbuild.sln "Debug|Win32"
-
--- python/branches/release27-maint-ttk-debug-on-xp5/Tools/buildbot/clean.bat
+++
@@ -1,8 +0,0 @@
-@rem Used by the buildbot "clean" step.
-call "%VS90COMNTOOLS%vsvars32.bat"
-vcbuild /useenv PCbuild\kill_python.vcproj "Debug|Win32" && PCbuild\kill_python_d.exe
-@echo Deleting .pyc/.pyo files ...
-del /s Lib\*.pyc Lib\*.pyo
-cd PCbuild
-vcbuild /clean pcbuild.sln "Release|Win32"
-vcbuild /clean pcbuild.sln "Debug|Win32"
